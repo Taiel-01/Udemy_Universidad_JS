79b7adc2db4286e8d3a23dda0b7239934e6ba599
Taiel Nunes <taielalen56@hotmail.com>
2021-05-07 18:50:29 -0300

    Seccion 3 completa

---
+++ Seccion_3/03-07-OperadoresRelacionales.js
@@ -0,0 +1,16 @@
+//* Operadores relacionales
+
+let a = 3, b = 2, c = "3";
+
+
+let z = a < b;
+console.log(z);
+
+z = a <= b;
+console.log(z);
+
+z = a > b;
+console.log(z);
+
+z = a >= b;
+console.log(z);
