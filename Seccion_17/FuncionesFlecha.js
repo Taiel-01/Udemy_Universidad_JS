06aaabfb8283daaa633a77a97ad9b41f2950b708
Taiel Nunes <taielalen56@hotmail.com>
2021-05-10 16:43:32 -0300

    Seccion 17 completa

---
+++ Seccion_17/FuncionesFlecha.js
@@ -0,0 +1,12 @@
+let miFuncion = function () {
+    console.log('saludos desde mi funcion');
+}
+
+
+const miFuncionFlecha = () => {
+    console.log('saludos desde mi funcion flecha')
+}
+
+miFuncion();
+miFuncionFlecha();
+
