847ccac5d77edee0fe34c57c41149dccd72bec42
Taiel Nunes <taielalen56@hotmail.com>
2021-05-10 16:22:53 -0300

    Seccion 16 completa

---
+++ Seccion_16/ManejoErrores.js
@@ -0,0 +1,13 @@
+"use strict"
+try {
+    //x = 10;
+    miFuncion();
+}
+catch (error) {
+    console.log(error);
+}
+finally {
+    console.log('termina la revision de errores')
+}
+
+console.log('continuamos...');
